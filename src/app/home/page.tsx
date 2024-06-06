0c576030da2ff0c866bd0ad38df9829d3c6621d9
ivanbecerrq <becerraandres124@gmail.com>
2024-06-06 14:40:27 -0500

    rediseño de home

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,7 +1,8 @@
 'use client'
 
 import { useRouter } from 'next/navigation'
-import styles from './styles.module.scss'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function HomePage() {
   const router = useRouter()
@@ -11,68 +12,62 @@ export default function HomePage() {
     router.push('/') // Redireccionar al usuario a la página de inicio de sesión
   }
 
-  const books = [
-    {
-      id: 1,
-      image: '/path/to/image1.jpg',
-      title: 'A lo romano y Arrestado por alienígena',
-      genre: 'Comic',
-      available: true
-    },
-    {
-      id: 2,
-      image: '/path/to/image2.jpg',
-      title: 'Ecofuturismo',
-      genre: 'Cuentos SCI-FI',
-      available: true
-    },
-    {
-      id: 3,
-      image: '/path/to/image3.jpg',
-      title: 'Si des de casa veus un drac',
-      genre: 'Comic',
-      available: false
-    },
-    // Añadir más libros aquí...
-  ]
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  const fetchBooks = async () => {
+    try {
+      const response = await axios.get('/api/books');
+      console.log(response)
+      if (Array.isArray(response.data.data)) {
+        setBooks(response.data.data);
+      } else {
+        console.error('Books data is not an array:', response.data.data);
+      }
+    } catch (error) {
+      console.error('Failed to fetch books', error);
+    }
+  };
 
   return (
-    <main className={styles.main}>
-      <nav className={styles.nav}>
-        <div className={styles.buttonContainer}>
+    <main className="flex flex-col min-h-screen w-full bg-gray-900 text-white">
+      <nav className="w-full bg-gray-800 shadow-md p-4 flex justify-between items-center">
+        <div className="text-3xl font-extrabold font-serif">Atlas</div>
+        <div className="flex space-x-4">
           <button
             onClick={() => router.push('/books')}
-            className={`${styles.button} ${styles.booksButton}`}
+            className="px-4 py-2 bg-blue-500 text-white rounded-lg shadow hover:bg-blue-600 transition duration-300"
           >
             Libros
           </button>
           <button
             onClick={() => router.push('/loans')}
-            className={`${styles.button} ${styles.loansButton}`}
+            className="px-4 py-2 bg-green-500 text-white rounded-lg shadow hover:bg-green-600 transition duration-300"
           >
             Préstamos
           </button>
           <button
             onClick={handleLogout}
-            className={`${styles.button} ${styles.logoutButton}`}
+            className="px-4 py-2 bg-red-500 text-white rounded-lg shadow hover:bg-red-600 transition duration-300"
           >
             Cerrar Sesión
           </button>
         </div>
       </nav>
-      <section className={styles.newReleases}>
-        <h2>Novedades</h2>
-        <div className={styles.booksGrid}>
+      <section className="flex-grow w-full p-8 bg-gray-800">
+        <h2 className="text-3xl font-bold text-white mb-4">Novedades</h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
           {books.map(book => (
-            <div key={book.id} className={styles.bookCard}>
-              <img src={book.image} alt={book.title} className={styles.bookImage} />
-              <div className={styles.bookDetails}>
-                <h3 className={styles.bookTitle}>{book.title}</h3>
-                <p className={styles.bookGenre}>{book.genre}</p>
-                <p className={book.available ? styles.bookAvailable : styles.bookUnavailable}>
+            <div key={book._id} className="bg-gray-700 rounded-lg shadow-lg p-4 hover:shadow-xl transition duration-300">
+              <div className="flex flex-col items-start">
+                <h3 className="text-xl font-semibold text-white">{book.title}</h3>
+                <p className="text-gray-300">{book.genre}</p>
+                <p className={book.available ? 'text-green-500' : 'text-red-500'}>
                   {book.available ? 'Disponible' : 'No disponible'}
                 </p>
-                <button className={styles.detailsButton}>Detalles</button>
               </div>
             </div>
           ))}
